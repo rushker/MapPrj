de97d7cd305daed50347b396b4ef6931a5d47380
rushker <altiosle102@gmail.com>
2025-05-12 09:47:50 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 // src/App.jsx
-import React from 'react';
-import AppRouter from './router';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 import { Toaster } from 'react-hot-toast';
 
 export default function App() {
   return (
     <>
-      <AppRouter />
+      <RouterProvider router={router} />
       <Toaster position="top-right" />
     </>
   );
 }
+
