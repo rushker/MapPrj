70bb6d678fa932b699c9f87bb13970b3f65bfea3
rushker <altiosle102@gmail.com>
2025-05-26 16:37:24 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 // src/App.jsx
+// src/App.jsx (đã sửa)
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PostMapPage from './pages/PostMapPage';
 import ViewMapPage from './pages/ViewMapPage';
+import ManagerPage from './pages/ManagerPage'; // Thêm import này
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/projects" element={<ManagerPage />} />
         <Route path="/projects/:projectId/areas/:areaId/edit" element={<PostMapPage />} />
         <Route path="/projects/:projectId/areas/:areaId/view" element={<ViewMapPage />} />
       </Routes>
