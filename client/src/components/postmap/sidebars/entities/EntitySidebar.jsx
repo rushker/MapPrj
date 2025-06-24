40ba392696bface814a5c4692eff6d4936741c3b
rushker <altiosle102@gmail.com>
2025-06-24 16:18:59 +0700

    Update EntitySidebar.jsx

--- client/src/components/postmap/sidebars/entities/EntitySidebar.jsx
+++ client/src/components/postmap/sidebars/entities/EntitySidebar.jsx
@@ -6,8 +6,12 @@ export default function EntitySidebar(props) {
   const { entity } = props;
 
   if (!entity || !entity.type) {
-  return <div className="p-4 text-red-500">Không có thông tin entity hoặc thiếu 'type'</div>;
-}
+    return (
+      <div className="p-4 text-red-500">
+        Không có thông tin entity hoặc thiếu 'type'
+      </div>
+    );
+  }
 
   switch (entity.type) {
     case 'polygon':
@@ -17,7 +21,7 @@ export default function EntitySidebar(props) {
     default:
       return (
         <div className="p-4 text-red-500">
-          ❌ Không hỗ trợ entity loại: <strong>{entity.type}</strong>
+          Không hỗ trợ entity loại: <strong>{entity.type}</strong>
         </div>
       );
   }
